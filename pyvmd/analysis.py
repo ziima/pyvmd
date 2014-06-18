dc327f2f34e99fbc8dbe86a1625995529a302d8c
Vlastimil Zíma <vlastimil.zima@gmail.com>
2014-06-18 14:28:24 +0200

    Add function to find hydrogen bonds

---
+++ pyvmd/analysis.py
@@ -0,0 +1,83 @@
+"""
+Utilities for structure analysis.
+"""
+import itertools
+
+import VMD
+
+from . import measure
+from .objects import Selection
+
+
+class HydrogenBond(object):
+    """
+    Represents hydrogen bond.
+
+    @ivar: Hydrogen donor atom
+    @ivar: Hydrogen atom
+    @ivar: Hydrogen acceptor atom
+    """
+    def __init__(self, donor, hydrogen, acceptor):
+        self.donor = donor
+        self.hydrogen = hydrogen
+        self.acceptor = acceptor
+
+    def __repr__(self):
+        return '<%s: %s--%s..%s>' % (type(self).__name__, self.donor, self.hydrogen, self.acceptor)
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.donor == other.donor and self.hydrogen == other.hydrogen and \
+            self.acceptor == other.acceptor
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+def _get_bonds(donor, acceptor, donor_hydrogens, angle):
+    # Utility function which finds hydrogen bonds between donor atom and acceptor atom
+    hydrogens = (a for a in donor.bonded if a in donor_hydrogens)
+    for hydrogen in hydrogens:
+        # Check the angle. If it's big enough, then it is a hydrogen bond
+        if measure.angle(donor, hydrogen, acceptor) >= angle:
+            yield HydrogenBond(donor, hydrogen, acceptor)
+
+
+def hydrogen_bonds(donors, acceptors=None, distance=3.0, angle=135):
+    """
+    Returns iterator of hydrogen bonds between the selections.
+
+    @param donors: Hydrogen donors selection
+    @type donors: Selection
+    @param acceptors: Hydrogen acceptors selection
+    @type donors: Selection or None
+    @param distance: Maximal distance between donor and acceptor
+    @param angle: Minimal angle in degrees between donor, hydrogen and acceptor
+    @rtype: Generator of HydrogenBond objects
+    """
+    assert isinstance(donors, Selection)
+    assert acceptors is None or isinstance(acceptors, Selection)
+    assert isinstance(distance, (int, float, long)) and distance >= 0
+    assert isinstance(angle, (int, float, long)) and 0 <= angle <= 180
+
+    # Remove hydrogen atoms from selection. This can be done safely, hydrogens are never donors.
+    donor_heavy = Selection('(%s) and noh' % donors.selection, donors.molecule, donors.frame)
+    # Create selections of hydrogens for donor molecule. This will be used to find the hydrogen involved in the bond.
+    donor_hydrogens = Selection('hydrogen', donors.molecule, donors.frame)
+    if acceptors is None:
+        # Acceptor is same as donor, just copy
+        acceptor_heavy = donor_heavy
+        acceptor_hydrogens = donor_hydrogens
+    else:
+        # Acceptor is not the same as donor. Make same selections as for donor.
+        acceptor_heavy = Selection('(%s) and noh' % acceptors.selection, acceptors.molecule, acceptors.frame)
+        acceptor_hydrogens = Selection('hydrogen', acceptors.molecule, acceptors.frame)
+
+    for donor, acceptor in donor_heavy.contacts(acceptor_heavy, distance):
+        for hbond in _get_bonds(donor, acceptor, donor_hydrogens, angle):
+            yield hbond
+        # If acceptors and donors share atoms, contacts return pair only once.
+        # Check if donor and acceptors can have opposite roles.
+        if donor in acceptor_heavy and acceptor in donor_heavy:
+            # Donor can be acceptor and acceptor can be donor, check the hydrogen bonds.
+            for hbond in _get_bonds(acceptor, donor, acceptor_hydrogens, angle):
+                yield hbond
---
+++ pyvmd/tests/test_analysis.py
@@ -0,0 +1,43 @@
+"""
+Tests for analysis.
+"""
+import unittest
+
+import VMD
+
+from pyvmd.analysis import hydrogen_bonds, HydrogenBond
+from pyvmd.objects import Atom, Selection
+from .utils import data
+
+
+class TestAnalysis(unittest.TestCase):
+    """
+    Test analysis utilities.
+    """
+    def setUp(self):
+        molid = VMD.molecule.load('psf', data('water.psf'), 'pdb', data('water.pdb'))
+        self.molid = molid
+
+    def tearDown(self):
+        # Delete all molecules
+        for molid in VMD.molecule.listall():
+            VMD.molecule.delete(molid)
+
+    def test_hydrogen_bonds(self):
+        # Test `hydrogen_bonds` function
+        sel = Selection('noh')
+
+        result = [HydrogenBond(Atom(18), Atom(19), Atom(9)), HydrogenBond(Atom(9), Atom(11), Atom(6)),
+                  HydrogenBond(Atom(6), Atom(7), Atom(15))]
+        self.assertEqual(list(hydrogen_bonds(sel)), result)
+        self.assertEqual(list(hydrogen_bonds(sel, sel)), result)
+        result = [HydrogenBond(Atom(18), Atom(19), Atom(9)), HydrogenBond(Atom(9), Atom(11), Atom(6)),
+                  HydrogenBond(Atom(6), Atom(7), Atom(9)), HydrogenBond(Atom(6), Atom(7), Atom(15))]
+        self.assertEqual(list(hydrogen_bonds(sel, angle=75)), result)
+        self.assertEqual(list(hydrogen_bonds(sel, angle=180)), [])
+        self.assertEqual(list(hydrogen_bonds(sel, distance=2)), [])
+
+        # If the selections do not share same atoms, check the hydrogen bonds are returned only in correct direction
+        sel1 = Selection('index 6')
+        sel2 = Selection('index 9')
+        self.assertEqual(list(hydrogen_bonds(sel1, sel2, angle=75)), [HydrogenBond(Atom(6), Atom(7), Atom(9))])
